257241801c9a3117f8d9213e198289b4c37d2b5f
ojha-url <mayank.ojha@gmail.com>
2017-02-08 21:06:19 -0500

    Added layers, refactored addLayer()

--- src/routes/landing/landing.js
+++ src/routes/landing/landing.js
@@ -9,6 +9,114 @@ export class Landing {
     this.controls = [];
   }
 
+  setSource(source, source_type, source_data) {
+    switch (source) {
+      case 'mapbox':
+        return {
+          'type': source_type,
+          'url': 'mapbox://asbarve.' + source_data
+        };
+      case 'geojson_url':
+        return {
+          'type': source_type,
+          'data': source_data
+        };
+    }
+  }
+
+  addLayer(group_no, label, layer_id, type, source, source_type, source_data, source_layer, paint_properties, visibility) {
+    var self = this;
+    if (visibility === 'visible') {
+      self.controlGroups[group_no].controls.push({name: label, id: layer_id, class: 'layerControls active'});
+    } else {
+      self.controlGroups[group_no].controls.push({name: label, id: layer_id, class: 'layerControls'});
+    }
+    switch (type) {
+      case 'fill':
+        return self.map.addLayer({
+          'id': layer_id,
+          'type': type,
+          'source': self.setSource(source, source_type, source_data),
+          'source-layer': source_layer,
+          'layout': {
+            'visibility': visibility
+          },
+          'paint': paint_properties
+        });
+      case 'line':
+        return self.map.addLayer({
+          'id': layer_id,
+          'type': type,
+          'source': self.setSource(source, source_type, source_data),
+          'source-layer': source_layer,
+          'layout': {
+            'visibility': visibility
+          },
+          'paint': paint_properties
+        });
+      case 'circle':
+        return self.map.addLayer({
+          'id': layer_id,
+          'type': type,
+          'source': self.setSource(source, source_type, source_data),
+          'source-layer': source_layer,
+          'layout': {
+            'visibility': visibility
+          },
+          'paint': paint_properties
+        });
+      case 'fill-extrusion':
+        return self.map.addLayer({
+          'id': layer_id,
+          'type': type,
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'minzoom': 10,
+          'layout': {
+            'visibility': visibility
+          },
+          'paint': paint_properties
+        });
+    }
+  }
+
+  attached() {
+    var self = this;
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNiYXJ2ZSIsImEiOiI4c2ZpNzhVIn0.A1lSinnWsqr7oCUo0UMT7w';
+    self.map = new mapboxgl.Map({
+      attributionControl: false,
+      container: 'map_wrapper',
+      center: [-80.25, 26.15],
+      zoom: 10,
+      style: 'mapbox://styles/mapbox/dark-v9',
+      hash: false
+    });
+    self.map.on('load', () => {
+      // Push object to self.controlGroups only if atleast one layer is toggleable
+      //Control group '0'
+      self.controlGroups.push({name: 'Flood Hazard Extents', id: 'fld_haz_ext', controls: []});
+      self.addLayer('0', 'Hazard AO', 'FLDHAO', 'fill', 'mapbox', 'vector', '1eqmjn9o', 'FLDHAO', {'fill-color': '#31aade', 'fill-opacity': 1}, 'none');
+      self.addLayer('0', 'Hazard AE', 'FLDHAE', 'fill', 'mapbox', 'vector', '4cou1y2j', 'FLDHAE', {'fill-color': '#31aade', 'fill-opacity': 0.8}, 'none');
+      self.addLayer('0', 'Hazard AH', 'FLDHAH', 'fill', 'mapbox', 'vector', '758t0cbw', 'FLDHAH', {'fill-color': '#31aade', 'fill-opacity': 0.5}, 'none');
+      self.addLayer('0', 'Hazard X', 'FLDHX', 'fill', 'mapbox', 'vector', '44qg0o2f', 'FLDHX', {'fill-color': '#31aade', 'fill-opacity': 0.2}, 'none');
+      self.addLayer('0', 'Hazard VE', 'FLDHVE', 'fill', 'mapbox', 'vector', 'b0mn3fbb', 'FLDHVE', {'fill-color': '#31aade', 'fill-opacity': 0.1}, 'none');
+
+      //Control group '1'
+      self.controlGroups.push({name: 'Water infrastructure', id: 'wtr_inf', controls: []});
+      self.addLayer('1', 'Water bodies', 'S_WTR', 'fill', 'mapbox', 'vector', 'c5vfi3yr', 'S_WTR', {'fill-color': '#1a1a1a'}, 'none'); //Match mapbox style water color
+      self.addLayer('1', 'City boundaries', 'city_boundary', 'line', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/cities.geojson', '', {'line-color': '#aaaaaa'}, 'none');
+      self.addLayer('1', 'Salt water intrusion', 'salt_water', 'line', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/saltwaterFlorida.geojson', '', {'line-color': '#c1272d', 'line-width': 3}, 'visible');
+
+      //Control group '2'
+      self.controlGroups.push({name: 'Physical infrastructure', id: 'phy_inf', controls: []});
+      self.addLayer('2', '3D buildings', '3d_buildings', 'fill-extrusion', null, null, null, null, {'fill-extrusion-color': '#aaaaaa', 'fill-extrusion-height': {'type': 'identity', 'property': 'height'}, 'fill-extrusion-base': {'type': 'identity', 'property': 'min_height'}, 'fill-extrusion-opacity': 0.8}, 'none');
+      self.addLayer('2', 'Future landuse', 'landuse', 'fill', 'mapbox', 'vector', '2kwgic7s', 'landuse', {'fill-color': lbcsCodes, 'fill-opacity': 0.8}, 'none');
+      self.addLayer('2', 'RedCross locations', 'red_cross', 'circle', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/redCross.geojson', '', {'circle-color': '#c1272d', 'circle-radius': 6, 'circle-opacity': 0.8}, 'none');
+    });
+  }
+
+  //Show / hide toolbar
   toggleToolbar() {
     var toggleSpeed = 200;
     var toolBarWidth = $('#toolbar_wrapper').width();
@@ -42,6 +150,7 @@ export class Landing {
     }
   }
 
+  //Toggle layer visibility parameter & toggle buttons appearance
   toggleLayer(layer_id) {
     $('#toggle_'+layer_id).toggleClass('active');
     var visibility = this.map.getLayoutProperty(layer_id, 'visibility');
@@ -52,101 +161,9 @@ export class Landing {
     }
   }
 
-  addFillLayer(group_no, label, code, layer_id, color, opacity, toggleable, visibility) {
-    if (toggleable) {
-      this.controlGroups[group_no].controls.push({name: label, id: layer_id});
-    }
-    return this.map.addLayer({
-      'id': layer_id,
-      'type': 'fill',
-      'source': {
-        'type': 'vector',
-        'url': 'mapbox://asbarve.' + code //TODO: catch get tile error, shows in console when viewing tiles outside of layer bounds
-      },
-      'source-layer': layer_id,
-      'layout': {
-        'visibility': visibility
-      },
-      'paint': {
-        'fill-color': color,
-        'fill-opacity': opacity
-      }
-    });
-  }
-
-  add3dLayer(group_no, label, layer_id, visibility) {
-    this.controlGroups[group_no].controls.push({name: label, id: layer_id});
-    return this.map.addLayer({
-     'id': layer_id,
-     'source': 'composite',
-     'source-layer': 'building',
-     'filter': ['==', 'extrude', 'true'],
-     'type': 'fill-extrusion',
-     'minzoom': 10,
-     'layout': {
-       'visibility': visibility
-     },
-     'paint': {
-       'fill-extrusion-color': '#aaa',
-       'fill-extrusion-height': {
-         'type': 'identity',
-         'property': 'height'
-       },
-       'fill-extrusion-base': {
-         'type': 'identity',
-         'property': 'min_height'
-       },
-       'fill-extrusion-opacity': 0.8
-     }
-   });
-  }
-
-  attached() {
-    var self = this;
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNiYXJ2ZSIsImEiOiI4c2ZpNzhVIn0.A1lSinnWsqr7oCUo0UMT7w';
-    self.map = new mapboxgl.Map({
-      attributionControl: false,
-      container: 'map_wrapper',
-      center: [-80.25, 26.15],
-      zoom: 10,
-      style: 'mapbox://styles/mapbox/dark-v9',
-      hash: false
-    });
-    self.map.on('load', () => {
-      // Push object to self.controlGroups only if atleast one layer is toggleable
-      //Control group '0'
-      self.controlGroups.push({name: 'Flood Hazard Extents', id: 'fld_haz_ext', controls: []});
-      self.addFillLayer('0', 'Hazard AO', '1eqmjn9o', 'FLDHAO', '#31aade', 1, true, 'none');
-      self.addFillLayer('0', 'Hazard AE', '4cou1y2j', 'FLDHAE', '#31aade', 0.8, true, 'none');
-      self.addFillLayer('0', 'Hazard AH', '758t0cbw', 'FLDHAH', '#31aade', 0.5, true, 'none');
-      self.addFillLayer('0', 'Hazard X', '44qg0o2f', 'FLDHX', '#31aade', 0.2, true, 'none');
-      self.addFillLayer('0', 'Hazard VE', 'b0mn3fbb', 'FLDHVE', '#31aade', 0.1, true, 'none');
-
-      //Control group '1'
-      self.controlGroups.push({name: 'Water infrastructure', id: 'wtr_inf', controls: []});
-      self.addFillLayer('1', 'Water bodies', 'c5vfi3yr', 'S_WTR', '#1a1a1a', 1, true, 'none'); //Match mapbox style water color
-
-      //Control group '2'
-      self.controlGroups.push({name: 'Physical infrastructure', id: 'phy_inf', controls: []});
-      self.add3dLayer('2', '3D buildings', '3d_buildings', 'none');
-      self.addFillLayer('2', 'Future landuse', '2kwgic7s', 'landuse', lbcsCodes, 0.75, true, 'none');
-    });
-  }
-
-  //Toggle only target group
+  //Toggle toolbar layer groups
   toggleGroup(group_name) {
     $('#group_' + group_name).slideToggle("fast");
     $('#toggle_' + group_name + ' > i').toggleClass("active");
   }
-
-  //Toggle all open + target group
-  /*toggleGroup(group_name) {
-    $('.groupWrapper:not(#group_' + group_name + ')').slideUp("fast");
-    $('#group_' + group_name).slideToggle("fast");
-    if ($('.groupToggle:not(#toggle_' + group_name + ') > i.icon-up-open').hasClass('active')) {
-      $('.groupToggle:not(#toggle_' + group_name + ') > i.icon-up-open').removeClass('active');
-      $('.groupToggle:not(#toggle_' + group_name + ') > i.icon-down-open').addClass('active');
-    }
-    $('#toggle_' + group_name + ' > i').toggleClass("active");
-  }*/
 }
