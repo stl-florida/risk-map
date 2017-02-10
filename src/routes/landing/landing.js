6140bf60fbf9b815d28ca86cc62c528e2a0bf519
ojha-url <mayank.ojha@gmail.com>
2017-02-09 20:02:32 -0500

    Implemented tooltips for layers, refactored layer settings input

--- src/routes/landing/landing.js
+++ src/routes/landing/landing.js
@@ -1,7 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import {HttpClient} from 'aurelia-http-client';
 import * as topojson from 'topojson-client';
-import lbcsCodes from '../../styles/lbcs';
+import layers from './layers';
 
 export class Landing {
   constructor() {
@@ -9,75 +9,69 @@ export class Landing {
     this.controls = [];
   }
 
-  setSource(source, source_type, source_data) {
-    switch (source) {
-      case 'mapbox':
-        return {
-          'type': source_type,
-          'url': 'mapbox://asbarve.' + source_data
-        };
-      case 'geojson_url':
-        return {
-          'type': source_type,
-          'data': source_data
-        };
+  setSource(name, type, data) {
+    switch (name) {
+      case 'mapbox': return {
+        'type': type,
+        'url': 'mapbox://asbarve.' + data
+      };
+      case 'geojson_url': return {
+        'type': type,
+        'data': data
+      };
     }
   }
 
-  addLayer(group_no, label, layer_id, type, source, source_type, source_data, source_layer, paint_properties, visibility) {
+  addLayer(properties) {
     var self = this;
-    if (visibility === 'visible') {
-      self.controlGroups[group_no].controls.push({name: label, id: layer_id, class: 'layerControls active'});
+    if (properties.visibility === 'visible') {
+      self.controlGroups[properties.group_no].controls.push({name: properties.label, id: properties.layer_id, tooltip: properties.tooltip_text, class: 'layerControls active'});
     } else {
-      self.controlGroups[group_no].controls.push({name: label, id: layer_id, class: 'layerControls'});
+      self.controlGroups[properties.group_no].controls.push({name: properties.label, id: properties.layer_id, tooltip: properties.tooltip_text, class: 'layerControls'});
     }
-    switch (type) {
-      case 'fill':
-        return self.map.addLayer({
-          'id': layer_id,
-          'type': type,
-          'source': self.setSource(source, source_type, source_data),
-          'source-layer': source_layer,
-          'layout': {
-            'visibility': visibility
-          },
-          'paint': paint_properties
-        });
-      case 'line':
-        return self.map.addLayer({
-          'id': layer_id,
-          'type': type,
-          'source': self.setSource(source, source_type, source_data),
-          'source-layer': source_layer,
-          'layout': {
-            'visibility': visibility
-          },
-          'paint': paint_properties
-        });
-      case 'circle':
-        return self.map.addLayer({
-          'id': layer_id,
-          'type': type,
-          'source': self.setSource(source, source_type, source_data),
-          'source-layer': source_layer,
-          'layout': {
-            'visibility': visibility
-          },
-          'paint': paint_properties
-        });
-      case 'fill-extrusion':
-        return self.map.addLayer({
-          'id': layer_id,
-          'type': type,
-          'source': 'composite',
-          'source-layer': 'building',
-          'filter': ['==', 'extrude', 'true'],
-          'minzoom': 10,
-          'layout': {
-            'visibility': visibility
-          },
-          'paint': paint_properties
-        });
+    switch (properties.type) {
+      case 'fill': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': self.setSource(properties.source.name, properties.source.type, properties.source.data),
+        'source-layer': properties.source.layer,
+        'layout': {
+          'visibility': properties.visibility
+        },
+        'paint': properties.paint
+      });
+      case 'line': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': self.setSource(properties.source.name, properties.source.type, properties.source.data),
+        'source-layer': properties.source.layer,
+        'layout': {
+          'visibility': properties.visibility
+        },
+        'paint': properties.paint
+      });
+      case 'circle': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': self.setSource(properties.source.name, properties.source.type, properties.source.data),
+        'source-layer': properties.source.layer,
+        'layout': {
+          'visibility': properties.visibility
+        },
+        'paint': properties.paint
+      });
+      case 'fill-extrusion': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'minzoom': 10,
+        'layout': {
+          'visibility': properties.visibility
+        },
+        'paint': properties.paint
+      });
     }
   }
 
@@ -96,23 +90,26 @@ export class Landing {
       // Push object to self.controlGroups only if atleast one layer is toggleable
       //Control group '0'
       self.controlGroups.push({name: 'Flood Hazard Extents', id: 'fld_haz_ext', controls: []});
-      self.addLayer('0', 'Hazard VE', 'FLDHVE', 'fill', 'mapbox', 'vector', 'b0mn3fbb', 'FLDHVE', {'fill-color': '#31aade', 'fill-opacity': 1}, 'none');
-      self.addLayer('0', 'Hazard AO', 'FLDHAO', 'fill', 'mapbox', 'vector', '1eqmjn9o', 'FLDHAO', {'fill-color': '#31aade', 'fill-opacity': 0.8}, 'none');
-      self.addLayer('0', 'Hazard AE', 'FLDHAE', 'fill', 'mapbox', 'vector', '4cou1y2j', 'FLDHAE', {'fill-color': '#31aade', 'fill-opacity': 0.5}, 'none');
-      self.addLayer('0', 'Hazard AH', 'FLDHAH', 'fill', 'mapbox', 'vector', '758t0cbw', 'FLDHAH', {'fill-color': '#31aade', 'fill-opacity': 0.2}, 'none');
-      self.addLayer('0', 'Hazard X', 'FLDHX', 'fill', 'mapbox', 'vector', '44qg0o2f', 'FLDHX', {'fill-color': '#31aade', 'fill-opacity': 0.1}, 'none');
+      self.addLayer(layers.fldhaz_ve);
+      self.addLayer(layers.fldhaz_ao);
+      self.addLayer(layers.fldhaz_ae);
+      self.addLayer(layers.fldhaz_ah);
+      self.addLayer(layers.fldhaz_x);
 
       //Control group '1'
-      self.controlGroups.push({name: 'Water infrastructure', id: 'wtr_inf', controls: []});
-      self.addLayer('1', 'Water bodies', 'S_WTR', 'fill', 'mapbox', 'vector', 'c5vfi3yr', 'S_WTR', {'fill-color': '#1a1a1a'}, 'none'); //Match mapbox style water color
-      self.addLayer('1', 'City boundaries', 'city_boundary', 'line', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/cities.geojson', '', {'line-color': '#aaaaaa'}, 'none');
-      self.addLayer('1', 'Salt water intrusion', 'salt_water', 'line', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/saltwaterFlorida.geojson', '', {'line-color': '#c1272d', 'line-width': 3}, 'visible');
+      self.controlGroups.push({name: 'Water Infrastructure', id: 'wtr_inf', controls: []});
+      self.addLayer(layers.water_bodies);
+      self.addLayer(layers.salt_water_intrusion);
 
       //Control group '2'
-      self.controlGroups.push({name: 'Physical infrastructure', id: 'phy_inf', controls: []});
-      self.addLayer('2', '3D buildings', '3d_buildings', 'fill-extrusion', null, null, null, null, {'fill-extrusion-color': '#aaaaaa', 'fill-extrusion-height': {'type': 'identity', 'property': 'height'}, 'fill-extrusion-base': {'type': 'identity', 'property': 'min_height'}, 'fill-extrusion-opacity': 0.8}, 'none');
-      self.addLayer('2', 'Future landuse', 'landuse', 'fill', 'mapbox', 'vector', '2kwgic7s', 'landuse', {'fill-color': lbcsCodes, 'fill-opacity': 0.8}, 'none');
-      self.addLayer('2', 'RedCross locations', 'red_cross', 'circle', 'geojson_url', 'geojson', 'https://raw.githubusercontent.com/stl-florida/data-layers/master/redCross.geojson', '', {'circle-color': '#c1272d', 'circle-radius': 6, 'circle-opacity': 0.8}, 'none');
+      self.controlGroups.push({name: 'City Data', id: 'phy_inf', controls: []});
+      self.addLayer(layers.future_landuse);
+      self.addLayer(layers.city_boundaries);
+
+      //Control group '3'
+      self.controlGroups.push({name: 'Physical Infrastructure', id: 'phy_inf', controls: []});
+      self.addLayer(layers.red_cross);
+      self.addLayer(layers.buildings);
     });
   }
 
@@ -165,5 +162,9 @@ export class Landing {
   toggleGroup(group_name) {
     $('#group_' + group_name).slideToggle("fast");
     $('#toggle_' + group_name + ' > i').toggleClass("active");
+  }
+
+  toggleTooltip(text) {
+    this.tooltip_text = text;
   }
 }
