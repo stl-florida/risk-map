4ced7499aabbce626fab3825472c2bcf00eb69f3
ojha-url <mayank.ojha@gmail.com>
2017-03-16 22:53:20 -0400

    Added wms layer for realtime data fetching off usgs

--- src/routes/landing/landing-utility.js
+++ src/routes/landing/landing-utility.js
@@ -1,27 +1,73 @@
-import LAYERS from './layers';
+import {LAYERS, WMS} from './layers';
 import CONFIG from '../../config';
 import {HttpClient} from 'aurelia-http-client';
 import * as topojson from 'topojson-client';
-//import {HttpClient, json} from 'aurelia-fetch-client';
+import Chart from 'chart';
+import $ from 'jquery';
 
 export class LandingUtility {
   constructor() {
     this.map = null;
     this.controlGroups = [];
     this.mapView = '2d'; //TODO: DELETE
+    this.area_query = ['age', 'hazard', 'storage', 'retention'];
+    this.sel_query_setting = 'age';
   }
 
-  setSource(name, type, data) {
-    switch (name) {
-      case 'mapbox': return {
-        'type': type,
-        'url': 'mapbox://asbarve.' + data
-      };
-      case 'geojson_url': return {
-        'type': type,
-        'data': data
-      };
+  getWMS(type, params) {
+    let queries = '';
+    let baseUrl;
+    let client = new HttpClient();
+    if (type === 'usgs_gw_monitoring') {
+      baseUrl = 'https://waterdata.usgs.gov/fl/nwis/uv?';
+      for (let [key, val] of params) {
+        queries = queries + key + '=' + val + '&';
+      }
     }
+    return new Promise((resolve, reject) => {
+      client.createRequest(queries)
+        .asGet()
+        .withBaseUrl(baseUrl)
+        .withInterceptor({
+          request(msg) {
+            // console.log(msg);
+          },
+          requestError(err) {
+            reject(err);
+          },
+          response(msg) {
+            // console.log(msg);
+            let rows = msg.response.split('\n');
+            let remove = [];
+            for (let row in rows) {
+              if (rows[row].indexOf('#') === 0) {
+                remove.push(row);
+              }
+            }
+            rows.splice(0, remove.length); //remove lines beginning with #
+            let keys = rows[0].split('\t'); //store headers
+            rows.splice(0, 2); //headers (2 rows)
+            for (let row in rows) {
+              let cols = rows[row].split('\t');
+              rows[row] = cols;
+            }
+            rows.splice(rows.length - 1, 1); //last row
+            let readings = [];
+            for (let i in rows) {
+              let single_entry = {};
+              for (let j in keys) {
+                single_entry[keys[j]] = rows[i][j];
+              }
+              readings.push(single_entry);
+            }
+            resolve(readings);
+          },
+          responseError(err) {
+            reject(err);
+          }
+        })
+        .send();
+    });
   }
 
   getData(end_point) {
@@ -63,6 +109,23 @@ export class LandingUtility {
         resolve(new_properties);
       });
     });
+  }
+
+  setSource(name, type, data) {
+    switch (name) {
+      case 'mapbox_user': return {
+        'type': type,
+        'url': 'mapbox://asbarve.' + data
+      };
+      case 'geojson_format': return {
+        'type': type,
+        'data': data
+      };
+      case 'mapbox_global': return {
+        'type': type,
+        'url': data
+      };
+    }
   }
 
   applySettings(properties) {
@@ -120,6 +183,26 @@ export class LandingUtility {
         },
         'paint': properties.paint
       });
+      case 'symbol': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': self.setSource(properties.source.name, properties.source.type, properties.source.data),
+        'layout': {
+          'icon-image': '{icon}',
+          'visibility': properties.visibility
+        },
+      });
+      // for contours, hillshade
+      /*case 'vector': return self.map.addLayer({
+        'id': properties.layer_id,
+        'type': properties.type,
+        'source': self.setSource(properties.source.name, properties.source.type, properties.source.data),
+        'source-layer': properties.source.layer,
+        'layout': {
+          'visibility': properties.visibility
+        },
+        'paint': properties.paint
+      });*/
     }
   }
 
@@ -137,6 +220,7 @@ export class LandingUtility {
 
   loadLayers() {
     var self = this;
+    //TODO: Structure LAYERS by group_no, use for.. of loop
     // Push object to self.controlGroups only if atleast one layer is toggleable
     //Control group '0'
     self.controlGroups.push({group_no: '0', name: 'Flood Hazard Extents', id: 'fld_haz_ext', controls: []});
@@ -164,6 +248,9 @@ export class LandingUtility {
     //Control group '4'
     self.controlGroups.push({group_no: '4', name: 'Local db test', id: 'lcl_db', controls: []});
     self.addLayer('/slosh', LAYERS.storm_surge);
+    self.addLayer(null, LAYERS.gw_wells);
+    //self.addLayer(null, LAYERS.contours);
+    //self.addLayer(null, LAYERS.hillshade);
   }
 
   pointQuery(end_point, data, content_type) {
@@ -221,11 +308,108 @@ export class LandingUtility {
     });
   }
 
+  drawChart(popup, well, wms_map) {
+    var self = this;
+    popup.setLngLat([well.dec_long_va, well.dec_lat_va])
+         .setHTML('<canvas id="chart"></canvas>')
+         .addTo(self.map);
+    var chart_data = {
+      labels : [],
+      datasets : [{
+        label: 'Ground water level',
+        fill: 'bottom',
+        lineTension: 0,
+        spanGaps: true,
+        backgroundColor: "rgba(151,187,205,0.2)",
+        borderColor: "rgba(151,187,205,1)",
+        pointRadius: 0,
+        data: []
+      }]
+    };
+    self.getWMS('usgs_gw_monitoring', wms_map)
+    .then(data => {
+      var chart_ctx = $('#chart').get(0).getContext('2d');
+
+      for (let entry in data) {
+        if (data[entry].datetime) {
+          chart_data.labels.push(data[entry].datetime);
+          var ts; // check with usgs what ts parameter stands for...
+          for (let key in data[entry]) {
+            if (key !== 'agency_cd' && key !== 'datetime' && key !== 'site_no' && key !== 'tz_cd') {
+              ts = key.toString().split('_')[0];
+              break;
+            }
+          }
+          chart_data.datasets[0].data.push(data[entry][ts + '_62610']); //code for broward
+        }
+      }
+      //console.log(chart_data);
+      var well_chart = new Chart(chart_ctx, {
+        type: 'line',
+        data: chart_data,
+        options: {
+          title: {
+            text: well.station_nm,
+            display: true
+          },
+          legend: {
+            display: false,
+            position: 'bottom'
+          },
+          bezierCurve: false,
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                labelString: 'm',
+              },
+              gridLines: {
+                zeroLineColor: 'rgba(0,0,0,1)'
+              },
+              ticks: {
+                max: well.alt_va,
+                min: well.alt_va - well.well_depth_va
+              }
+            }],
+            xAxes: [{
+              gridLines: {
+                drawOnChartArea: false,
+              },
+              ticks: {
+                minRotation: 40,
+                maxRotation: 40
+              },
+              type: 'time',
+              time: {
+                unit: 'hour',
+                unitStepSize: 6,
+                displayFormats: {
+                  'millisecond': 'HH:mm',
+                  'second': 'HH:mm',
+                  'minute': 'HH:mm',
+                  'hour': 'HH:mm',
+                  'day': 'HH:mm',
+                  'week': 'HH:mm',
+                  'month': 'HH:mm',
+                  'quarter': 'HH:mm',
+                  'year': 'HH:mm'
+                }
+              }
+            }]
+          },
+          tooltips: {
+            enabled: false
+          }
+        }
+      });
+    });
+  }
+
   showPopup(e, popup) {
     var self = this;
     var features = self.map.queryRenderedFeatures(e.point, {layers: ['red_cross', 'buildings']});
     var landuse = self.map.queryRenderedFeatures(e.point, {layers: ['landuse']});
     var fldhaz = self.map.queryRenderedFeatures(e.point, {layers: ['FLDHVE', 'FLDHAO', 'FLDHAE', 'FLDHAH', 'FLDHX']});
+    var markers = self.map.queryRenderedFeatures(e.point, {layers: ['gw_wells']});
     if (features.length) {
       var feature = features[0];
       var landuse_info = 'Undefined';
@@ -251,6 +435,21 @@ export class LandingUtility {
           popup.setHTML('');
       }
       popup.addTo(self.map);
+    } else if (markers.length) {
+      var well = WMS.usgs_water.gw_wells.static[markers[0].properties.uid];
+      var wms_map = WMS.usgs_water.gw_wells.dynamic;
+      wms_map.set('site_no', markers[0].properties.uid);
+      var date;
+      if (well.construction_dt) {
+        date = well.construction_dt.toString().slice(0,4);
+      } else if (well.inventory_dt) {
+        date = well.inventory_dt.toString().slice(0,4);
+      } else {
+        date = "Unknown";
+      }
+      //.setHTML('Name: ' + well.station_nm + ', ' + well.map_nm + '<br>Ground surface altitude: ' + well.alt_va + ' m<br>Construction date: ' + date + '<br>Well depth: ' + well.well_depth_va + ' m')
+
+      self.drawChart(popup, well, wms_map);
     }
   }
 
@@ -434,7 +633,6 @@ export class LandingUtility {
         */
 
       } else if (feature.features[0].geometry.type === 'Polygon') {
-
         center = turf.centroid(feature.features[0]).geometry.coordinates;
         self.map.flyTo({center: center});
         var poly_coords = feature.features[0].geometry.coordinates[0];
@@ -448,41 +646,141 @@ export class LandingUtility {
             line_string = line_string + poly_coords[i][0] + ' ' + poly_coords[i][1] + ')';
           }
         }
-        self.areaQuery('/area/age', {coords: line_string}, 'application/json')
-        .then(topo_data => {
-          var geo_data = topojson.feature(topo_data, topo_data.objects.output);
-          self.map.addLayer({
-            'id': 'lots',
-            'type': 'fill',
-            'source': {
-              'type': 'geojson',
-              'data': geo_data
-            },
-            'source-layer': '',
-            'layout': {
-              'visibility': 'visible'
-            },
-            'paint': {
-              'fill-color': {
-                'property': 'year_built',
-                'type': 'exponential',
-                'stops': [
-                  [1900, '#49321a'],
-                  [1945, '#b78916'],
-                  [2000, '#8ad35f']
-                ]
+        if (self.sel_query_setting === 'age') {
+          self.map.removeLayer('age');
+          self.areaQuery('/area/age', {coords: line_string}, 'application/json')
+          .then(topo_data => {
+            var geo_data = topojson.feature(topo_data, topo_data.objects.output);
+            self.map.addLayer({
+              'id': 'age',
+              'type': 'fill',
+              'source': {
+                'type': 'geojson',
+                'data': geo_data
               },
-              'fill-opacity': {
-                'property': 'year_built',
-                'type': 'interval',
-                'stops': [
-                  [0, 0],
-                  [1, 0.6]
-                ]
+              'source-layer': '',
+              'layout': {
+                'visibility': 'visible'
+              },
+              'paint': {
+                'fill-color': {
+                  'property': 'year_built',
+                  'type': 'exponential',
+                  'stops': [
+                    [1900, '#49321a'],
+                    [1945, '#b78916'],
+                    [2000, '#8ad35f']
+                  ]
+                },
+                'fill-opacity': {
+                  'property': 'year_built',
+                  'type': 'interval',
+                  'stops': [
+                    [0, 0],
+                    [1, 0.6]
+                  ]
+                }
               }
-            }
+            });
           });
-        });
+        } else if (self.sel_query_setting === 'hazard') {
+          self.map.removeLayer('hazard');
+          self.areaQuery('/area/hazard', {coords: line_string}, 'application/json')
+          .then(topo_data => {
+            var geo_data = topojson.feature(topo_data, topo_data.objects.output);
+            self.map.addLayer({
+              'id': 'hazard',
+              'type': 'fill',
+              'source': {
+                'type': 'geojson',
+                'data': geo_data
+              },
+              'source-layer': '',
+              'layout': {
+                'visibility': 'visible'
+              },
+              'paint': {
+                'fill-color': {
+                  'property': 'fld_zone',
+                  'type': 'categorical',
+                  'stops': [
+                    ['VE', '#c1272d'],
+                    ['AO', '#cd5257'],
+                    ['AE', '#d97d81'],
+                    ['AH', '#e6a8ab'],
+                    ['X', '#f2d3d5']
+                  ]
+                },
+                'fill-opacity': 0.8,
+                'fill-outline-color': '#f7f7f7'
+              }
+            });
+          });
+        } else if (self.sel_query_setting === 'storage') {
+          self.map.removeLayer('storage');
+          self.areaQuery('/area/storage', {coords: line_string}, 'application/json')
+          .then(topo_data => {
+            var geo_data = topojson.feature(topo_data, topo_data.objects.output);
+            self.map.addLayer({
+              'id': 'storage',
+              'type': 'fill',
+              'source': {
+                'type': 'geojson',
+                'data': geo_data
+              },
+              'source-layer': '',
+              'layout': {
+                'visibility': 'none'
+              },
+              'paint': {
+                'fill-color': {
+                  'property': 'elev',
+                  'type': 'exponential',
+                  'stops': [
+                    [1, '#c7dcef'],
+                    [30, '#08306b']
+                  ]
+                },
+                'fill-opacity': 1
+              }
+            });
+          });
+        } else if (self.sel_query_setting === 'retention') {
+          self.map.removeLayer('retention');
+          self.areaQuery('/area/retention', {coords: line_string}, 'application/json')
+          .then(topo_data => {
+            var geo_data = topojson.feature(topo_data, topo_data.objects.output);
+            self.map.addLayer({
+              'id': 'retention',
+              'type': 'fill',
+              'source': {
+                'type': 'geojson',
+                'data': geo_data
+              },
+              'source-layer': '',
+              'layout': {
+                'visibility': 'visible'
+              },
+              'paint': {
+                'fill-color': {
+                  'property': 'val',
+                  'type': 'exponential',
+                  'stops': [
+                    [0, '#c7dcef'],
+                    [20, '#08306b']                  ]
+                },
+                'fill-opacity': {
+                  'property': 'val',
+                  'type': 'exponential',
+                  'stops': [
+                    [0, 0.1],
+                    [20, 1]
+                  ]
+                }
+              }
+            });
+          });
+        }
 
         /*
         landuse = self.map.queryRenderedFeatures(self.map.project(center), {layers: ['landuse']});
--- src/routes/landing/layers.js
+++ src/routes/landing/layers.js
@@ -1,6 +1,70 @@
 import lbcsCodes from '../../styles/lbcs';
 import CONFIG from '../../config';
 
+var WMS = {
+  usgs_water: {
+    gw_wells: {
+      static: {
+        "255807080224301": {"station_nm":"G  -1636","dec_lat_va":25.9685,"dec_long_va":-80.3784167,"map_nm":"","alt_va":6.2,"construction_dt":null,"inventory_dt":null,"well_depth_va":24},
+        "260010080085001": {"station_nm":"F  - 291","dec_lat_va":26.0031611,"dec_long_va":-80.1468528,"map_nm":"FT LAUDERDALE SOUTH","alt_va":9.16,"construction_dt":19390101,"inventory_dt":null,"well_depth_va":109},
+        "260032080135701": {"station_nm":"G  -1225","dec_lat_va":26.00925738,"dec_long_va":-80.2322705,"map_nm":"FT LAUDERDALE SOUTH","alt_va":9.1,"construction_dt":19621130,"inventory_dt":null,"well_depth_va":20},
+        "260040080104401": {"station_nm":"G  -2035","dec_lat_va":26.01147934,"dec_long_va":-80.1786577,"map_nm":"FT LAUDERDALE SOUTH","alt_va":13.2,"construction_dt":19710101,"inventory_dt":null,"well_depth_va":52},
+        "260053080105701": {"station_nm":"G  -1226","dec_lat_va":26.01509033,"dec_long_va":-80.1822689,"map_nm":"FT LAUDERDALE SOUTH","alt_va":9.1,"construction_dt":null,"inventory_dt":19621130,"well_depth_va":20},
+        "260219080141101": {"station_nm":"G  -1223","dec_lat_va":26.0393368,"dec_long_va":-80.2361012,"map_nm":"FT LAUDERDALE SOUTH","alt_va":6.31,"construction_dt":19621120,"inventory_dt":null,"well_depth_va":20},
+        "260325080113901": {"station_nm":"G  -2900","dec_lat_va":26.05777778,"dec_long_va":-80.19388889,"map_nm":"Fort Lauderdale Sout","alt_va":5.98,"construction_dt":19971201,"inventory_dt":19991013,"well_depth_va":114.5},
+        "260458080134801": {"station_nm":"G  -1221","dec_lat_va":26.08314348,"dec_long_va":-80.2297703,"map_nm":"FT LAUDERDALE SOUTH","alt_va":7.1,"construction_dt":19621120,"inventory_dt":null,"well_depth_va":20},
+        "260515080202101": {"station_nm":"G  - 617","dec_lat_va":26.08777778,"dec_long_va":-80.3385556,"map_nm":"COOPER CITY","alt_va":6.6,"construction_dt":19501115,"inventory_dt":19501115,"well_depth_va":29},
+        "260545080082001": {"station_nm":"G  - 561","dec_lat_va":26.09619829,"dec_long_va":-80.13865628,"map_nm":"FT LAUDERDALE SOUTH","alt_va":8.5,"construction_dt":19480209,"inventory_dt":19480209,"well_depth_va":20.3},
+        "260653080184901": {"station_nm":"G  -2034","dec_lat_va":26.0341111,"dec_long_va":-80.3853056,"map_nm":"COOPER CITY SW","alt_va":6.3,"construction_dt":19720101,"inventory_dt":null,"well_depth_va":22},
+        "260657080122301": {"station_nm":"S  - 329","dec_lat_va":26.11567778,"dec_long_va":-80.2063333,"map_nm":"FT LAUDERDALE SOUTH","alt_va":9.2,"construction_dt":1926,"inventory_dt":19400921,"well_depth_va":68},
+        "260752080084701": {"station_nm":"G  -1220","dec_lat_va":26.1310611,"dec_long_va":-80.14625,"map_nm":"FT LAUDERDALE NORTH","alt_va":7,"construction_dt":19621121,"inventory_dt":19621121,"well_depth_va":20},
+        "260821080185101": {"station_nm":"G  -2032","dec_lat_va":26.1386111,"dec_long_va":-80.3138611,"map_nm":"COOPER CITY","alt_va":6.6,"construction_dt":19720101,"inventory_dt":null,"well_depth_va":22},
+        "261141080163401": {"station_nm":"G  -2033","dec_lat_va":26.1956111,"dec_long_va":-80.2759167,"map_nm":"COOPER CITY NE","alt_va":11,"construction_dt":19720101,"inventory_dt":null,"well_depth_va":23},
+        "261147080114501": {"station_nm":"G  -2395","dec_lat_va":26.1965,"dec_long_va":-80.1958056,"map_nm":"FT LAUDERDALE NORTH","alt_va":9.4,"construction_dt":19840201,"inventory_dt":null,"well_depth_va":73},
+        "261434080071901": {"station_nm":"G  - 853","dec_lat_va":26.24319444,"dec_long_va":-80.1218333,"map_nm":"POMPANO BEACH","alt_va":19.7,"construction_dt":19600101,"inventory_dt":null,"well_depth_va":27},
+        "261441080111301": {"station_nm":"G  -1316","dec_lat_va":26.24480556,"dec_long_va":-80.1865833,"map_nm":"FT LAUDERDALE NORTH","alt_va":15.2,"construction_dt":null,"inventory_dt":null,"well_depth_va":15},
+        "261501080060701": {"station_nm":"G  -2147","dec_lat_va":26.25044444,"dec_long_va":-80.10125,"map_nm":"BOCA RATON","alt_va":9.1,"construction_dt":19740401,"inventory_dt":19740401,"well_depth_va":46},
+        "261534080165801": {"station_nm":"G  -2031","dec_lat_va":26.25944444,"dec_long_va":-80.26625,"map_nm":"CORAL SPRINGS NE","alt_va":12.65,"construction_dt":19720101,"inventory_dt":null,"well_depth_va":22},
+        "261641080064801": {"station_nm":"G  -2866","dec_lat_va":26.27805556,"dec_long_va":-80.11313889,"map_nm":"BOCA RATON","alt_va":17.24,"construction_dt":19960516,"inventory_dt":19960716,"well_depth_va":20},
+        "261708080090801": {"station_nm":"G  -1315","dec_lat_va":26.28533333,"dec_long_va":-80.1522222,"map_nm":"WEST DIXIE BEND","alt_va":15.4,"construction_dt":19690101,"inventory_dt":null,"well_depth_va":14},
+        "261734080111301": {"station_nm":"G  -1213","dec_lat_va":26.29294444,"dec_long_va":-80.18625,"map_nm":"WEST DIXIE BEND","alt_va":18.3,"construction_dt":19621120,"inventory_dt":null,"well_depth_va":15},
+        "261831080151301": {"station_nm":"G  -2739","dec_lat_va":26.30825,"dec_long_va":-80.253,"map_nm":"CORAL SPRINGS NE","alt_va":12.3,"construction_dt":null,"inventory_dt":19911207,"well_depth_va":21},
+        "261903080065601": {"station_nm":"G  -1260","dec_lat_va":26.31780556,"dec_long_va":-80.1153333,"map_nm":"BOCA RATON","alt_va":10.9,"construction_dt":19610801,"inventory_dt":null,"well_depth_va":90},
+        "261938080101001": {"station_nm":"G  -2852","dec_lat_va":26.32766667,"dec_long_va":-80.16925,"map_nm":"WEST DIXIE BEND","alt_va":15.82,"construction_dt":19791015,"inventory_dt":19791016,"well_depth_va":140}
+      },
+      dynamic: new Map([
+        ['cb_62610', 'on'],
+        ['format', 'rdb'],
+        ['site_no', null],
+        ['period', '1'],
+        ['begin_date', '2017-03-15'],
+        ['end_date', '2017-03-16']
+      ])
+    }
+  }
+};
+
+var getWMS_data = (feature_list, identifiers, marker_icon) => {
+  let features = [];
+  for (let feature in feature_list) {
+    features.push({
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [feature_list[feature][identifiers.long], feature_list[feature][identifiers.lat]]
+      },
+      "properties": {
+        "uid": feature,
+        "icon": marker_icon
+      }
+    });
+  }
+  return {
+    "type": "FeatureCollection",
+    "features": features
+  };
+};
+
 var LAYERS = {
   //Flood Hazard Extents
   'FLDHVE': {
@@ -9,7 +73,7 @@ var LAYERS = {
     layer_id: 'FLDHVE',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: 'b0mn3fbb',
       layer: 'FLDHVE',
@@ -28,7 +92,7 @@ var LAYERS = {
     layer_id: 'FLDHAO',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: '1eqmjn9o',
       layer: 'FLDHAO',
@@ -47,7 +111,7 @@ var LAYERS = {
     layer_id: 'FLDHAE',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: '4cou1y2j',
       layer: 'FLDHAE',
@@ -66,7 +130,7 @@ var LAYERS = {
     layer_id: 'FLDHAH',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: '758t0cbw',
       layer: 'FLDHAH',
@@ -85,7 +149,7 @@ var LAYERS = {
     layer_id: 'FLDHX',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: '44qg0o2f',
       layer: 'FLDHX',
@@ -105,7 +169,7 @@ var LAYERS = {
     layer_id: 'water_bodies',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: 'c5vfi3yr',
       layer: 'S_WTR',
@@ -123,7 +187,7 @@ var LAYERS = {
     layer_id: 'salt_water',
     type: 'line',
     source: {
-      name: 'geojson_url',
+      name: 'geojson_format',
       type: 'geojson',
       data: 'https://raw.githubusercontent.com/stl-florida/data-layers/master/saltwaterFlorida.geojson',
       layer: '',
@@ -142,7 +206,7 @@ var LAYERS = {
     layer_id: 'landuse',
     type: 'fill',
     source: {
-      name: 'mapbox',
+      name: 'mapbox_user',
       type: 'vector',
       data: '2kwgic7s',
       layer: 'landuse',
@@ -161,7 +225,7 @@ var LAYERS = {
     layer_id: 'city_boundaries',
     type: 'line',
     source: {
-      name: 'geojson_url',
+      name: 'geojson_format',
       type: 'geojson',
       data: 'https://raw.githubusercontent.com/stl-florida/data-layers/master/cities.geojson',
       layer: '',
@@ -179,7 +243,7 @@ var LAYERS = {
     layer_id: 'red_cross',
     type: 'circle',
     source: {
-      name: 'geojson_url',
+      name: 'geojson_format',
       type: 'geojson',
       data: 'https://raw.githubusercontent.com/stl-florida/data-layers/master/redCross.geojson',
       layer: '',
@@ -224,7 +288,7 @@ var LAYERS = {
     layer_id: 'storm_surge',
     type: 'fill',
     source: {
-      name: 'geojson_url',
+      name: 'geojson_format',
       type: 'geojson',
       data: null,
       layer: '',
@@ -245,7 +309,74 @@ var LAYERS = {
     },
     tooltip_text: 'Storm surge heights based on SLOSH model',
     visibility: 'none'
+  },
+  'gw_wells': {
+    group_no: '4',
+    label: 'USGS monitoring wells',
+    layer_id: 'gw_wells',
+    type: 'symbol',
+    source: {
+      name: 'geojson_format',
+      type: 'geojson',
+      data: getWMS_data(WMS.usgs_water.gw_wells.static, {lat: 'dec_lat_va', long: 'dec_long_va'}, 'marker-15'),
+      layer: null
+    },
+    tooltip_text: 'Realtime ground water monitoring feed',
+    visibility: 'visible'
+  },
+  'contours': {
+    group_no: '4',
+    label: 'Contours',
+    layer_id: 'contour',
+    type: 'line',
+    source: {
+      name: 'mapbox_global',
+      type: 'vector',
+      data: 'mapbox://mapbox.mapbox-terrain-v2',
+      layer: 'contour',
+    },
+    paint: {
+      "line-color": "#ff69b4",
+      "line-width": 1
+    },
+    tooltip_text: 'Contour lines',
+    visibility: 'none'
+  },
+  'hillshade': {
+    group_no: '4',
+    label: 'Hillshade',
+    layer_id: 'hillshade',
+    type: 'fill',
+    source: {
+      name: 'mapbox_global',
+      type: 'vector',
+      data: 'mapbox://mapbox.mapbox-terrain-v2',
+      layer: 'hillshade',
+    },
+    paint: {
+      'fill-color': {
+        'property': 'class',
+        'type': 'categorical',
+        'stops': [
+          ['shadow', '#ff0000'],
+          ['highlight', '#ffff00']
+        ]
+      },
+      'fill-opacity': {
+        'property': 'class',
+        'type': 'categorical',
+        'stops': [
+          ['shadow', 0.4],
+          ['highlight', 0.8]
+        ]
+      }
+    },
+    tooltip_text: 'Hillshade',
+    visibility: 'visible'
   }
 };
 
-module.exports = LAYERS;
+module.exports = {
+  LAYERS,
+  WMS
+};
